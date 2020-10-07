d3928597b9ac6de1e06529a2312a6411c3c473b8
William Reynolds <17876873+egadsthefuzz@users.noreply.github.com>
2020-10-08 00:59:39 +1100

    Latest tweak to use my own repo to pack it full of prevent delete

--- main.tf
+++ main.tf
@@ -3,17 +3,16 @@
 # be bootstrapped according to the simple yet essential procedure in
 # https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
 module "terraform_state_backend" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.26.1"
+  source     = "git::https://github.com/egadsthefuzz/terraform-aws-tfstate-backend.git?ref=safety-changes"
   namespace  = "egadsthefuzz"
   stage      = "test"
   name       = "terraform"
-  attributes = ["state"]
+  attributes = ["statestore"]
 
   terraform_backend_config_file_path = "."
   terraform_backend_config_file_name = "backend.tf"
   force_destroy                      = false
- }
-
+}
 # Set AWS as the provider and establish creds and default region
 provider aws {
   shared_credentials_file = var.shared_credentials
